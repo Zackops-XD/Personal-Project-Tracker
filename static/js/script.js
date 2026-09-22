7ac959ac7e68b8abda2d2a61116afa7775b61d96
Zack <bobbinzackops@gmail.com>
2026-09-22 11:11:01 +0700

    live search

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,3 @@
-console.log("JavaScript loaded!");
-
 const deleteForms = document.querySelectorAll(".delete-form");
 
 for (const form of deleteForms) {
@@ -17,3 +15,62 @@ for (const form of deleteForms) {
     });
 
 }
+
+const searchInput = document.querySelector("#project-search");
+
+const projectRows = document.querySelectorAll(
+    ".projects-table tbody tr"
+);
+
+const tableBody = document.querySelector(".projects-table tbody");
+
+searchInput.addEventListener("input", function() {
+
+    const searchText = searchInput.value;
+
+    const startsWithMatches = [];
+    const containsMatches = [];
+
+    for (const row of projectRows) {
+
+        const projectNameElement = row.querySelector(
+            ".project-name-link"
+        );
+
+        const projectName = projectNameElement.textContent.trim();
+
+        const isStartsWith = projectName
+            .toLowerCase()
+            .startsWith(searchText.toLowerCase());
+
+        const isContains = projectName
+            .toLowerCase()
+            .includes(searchText.toLowerCase());
+
+        if (isStartsWith) {
+
+            row.style.display = "";
+            startsWithMatches.push(row);
+
+        } else if (isContains) {
+
+            row.style.display = "";
+            containsMatches.push(row);
+
+        } else {
+
+            row.style.display = "none";
+
+        }
+
+    }
+
+    for (const row of startsWithMatches) {
+        tableBody.appendChild(row);
+    }
+
+    for (const row of containsMatches) {
+        tableBody.appendChild(row);
+    }
+
+});
